ad20437f4e703fff859a4123913b99b0700ccf29
sou <ocelot2828@gmail.com>
2023-08-07 20:02:53 +0900

    feat(core): major update

--- src/TextScaler/index.tsx
+++ src/TextScaler/index.tsx
@@ -1,219 +1,203 @@
-import React, {
-  useState,
-  useRef,
-  useLayoutEffect,
-  useCallback,
-  useEffect
-} from 'react'
+import React, { useState, useRef, useCallback, useEffect } from 'react'
 import styles from './styles.module.css'
-
-type SliderProperty = {
-  color?: string
-  borderColor?: string
-  handleColor?: string
-  handleBorderColor?: string
-}
 
 type TextScalerProps = {
   target: string
   size: number
   pathname?: string
   className?: string
-  sliderPosition?: number
-  sliderColor?: SliderProperty
+  classBox?: string
+  classSlider?: string
 }
 
+// Main functional,
+// this project of size variable is all font size value.
 export const TextScaler = ({
   target,
   size,
   pathname,
-  sliderPosition = 64,
   className,
-  sliderColor
+  classBox,
+  classSlider
 }: TextScalerProps) => {
-  const ref1 = useRef<HTMLDivElement>(null)
-  const ref2 = useRef<HTMLDivElement>(null)
-  const ref3 = useRef<HTMLDivElement>(null)
-  const [count, setCount] = useState(0)
-  const [isDragging, setIsDragging] = useState(false)
+  const refParent = useRef<HTMLDivElement>(null)
+  const refSwipe = useRef<HTMLDivElement>(null)
+  const [sizes, setSizes] = useState<number[]>([])
+  const [initialSize, setInitialSize] = useState(0)
   const [initialX, setInitialX] = useState(0)
   const [touchStartX, setTouchStartX] = useState(0)
-  const scaleFactor = 64 / 2 / size
-  const fontSizeRatio = 1 / scaleFactor
+  const [entryCount, setEntryCount] = useState(0)
+  const [applyCount, setApplyCount] = useState(0)
+  const [isDragging, setIsDragging] = useState(false)
+  const [hasMoving, setHasMoving] = useState(false)
 
+  // this is a calucration a variables.
+  const offset = initialSize - 10
+  const offsetFactor = 1 / initialSize
+  const scaleFactor = 100 / size
+  const fontSizeRatio = (1 / scaleFactor) * offsetFactor
+  const maxCountSize = size + initialSize
+  const minValue = -offset
+  const maxValue = size >= 14 ? size - offset : size
+
+  // The Core functional.
   const handleSetCount = useCallback(
     (delta: number) => {
-      setCount(prevCount => {
-        const newValue = prevCount + delta * fontSizeRatio
-        const clampedValue = Math.min(Math.max(newValue, -size), size)
-        return clampedValue
+      setEntryCount(prevCount => {
+        // this is handle the delta of handle as threshold, one step is 1px with fontSizeRatio.
+        const clampedValue = Math.max(
+          prevCount + delta * fontSizeRatio,
+          -initialSize / 5
+        )
+
+        // count is passed to the useLayoutEffect as variable in scale.
+        return Math.min(Math.max(clampedValue, minValue), maxValue)
       })
     },
-    [fontSizeRatio, size]
+    [fontSizeRatio, initialSize, maxValue, minValue]
   )
 
-  const handleClickCount = useCallback(
-    (val: number) => {
-      const clampedValue = Math.min(Math.max(val, -size), size)
-      setCount(clampedValue)
-    },
-    [size]
-  )
-
-  const [fontSizes, setFontSizes] = useState<number[]>([])
-
+  // This calculation refreshing as static render.
   useEffect(() => {
-    setCount(0)
+    setEntryCount(0)
   }, [pathname])
 
   useEffect(() => {
     const elements = document.querySelectorAll(target + ' *')
-    const text = document.querySelector('.' + styles.t)
     const fontSizeArray = []
 
     for (let i = 0; i < elements.length; i++) {
       const element = elements[i]
       const computedStyle = getComputedStyle(element)
-      const fontSizeInPx = parseFloat(computedStyle.fontSize)
-      fontSizeArray.push(isNaN(fontSizeInPx) ? 14 : fontSizeInPx)
+      const fontSize = parseFloat(computedStyle.fontSize)
+      fontSizeArray.push(isNaN(fontSize) ? 14 : fontSize)
     }
 
-    if (text) {
-      const textComputedStyle = getComputedStyle(text)
-      const fontSizeOfText = parseFloat(textComputedStyle.fontSize)
-      fontSizeArray.push(isNaN(fontSizeOfText) ? 14 : fontSizeOfText)
-    }
-
-    setFontSizes(fontSizeArray)
+    setSizes(fontSizeArray)
   }, [pathname, target])
 
-  useLayoutEffect(() => {
+  // The scaling the sizes values individually set a value.
+  useEffect(() => {
+    const scale = 1 + entryCount / 10 // use the entryCount variable.
     const elements = document.querySelectorAll(target + ' *')
-    const text = document.querySelector('.' + styles.t) as HTMLElement
-    const scale = 1 + count / 10
 
     for (let i = 0; i < elements.length; i++) {
       const element = elements[i] as HTMLElement
-      const initialFontSize = fontSizes[i]
-      const scaleFontSize = initialFontSize * scale
-      element.style.setProperty(
-        'font-size',
-        scaleFontSize < 10 ? '10px' : scaleFontSize + 'px'
-      )
+      const initialFontSize = sizes[i]
+      let scaleFontSize = initialFontSize * scale
+
+      if (scaleFontSize <= 10) scaleFontSize = 10
+
+      scaleFontSize = Math.min(scaleFontSize, maxCountSize)
+      element.style.setProperty('font-size', Math.round(scaleFontSize) + 'px')
+      console.log(Math.floor(scaleFontSize))
     }
 
-    if (text) {
-      const initialFontSize = fontSizes[fontSizes.length - 1]
-      const scaleFontSize = initialFontSize * scale
-      text.style.setProperty(
-        'font-size',
-        scaleFontSize < 10 ? '10px' : scaleFontSize + 'px'
-      )
-    }
-  }, [count, fontSizes, target])
+    const body = document.querySelector('body') as HTMLElement
+    const computedStyleBody = getComputedStyle(body)
+    const bodySize = parseFloat(computedStyleBody.fontSize)
+    setInitialSize(bodySize) // mount the initialSize as factors calucration.
 
-  const handleWheel = useCallback(
-    (e: WheelEvent) => {
-      const delta = e.deltaY * 0.1
-      handleSetCount(-delta)
-    },
-    [handleSetCount]
-  )
-
-  const enterControll = (e: Event) => {
-    e.preventDefault()
-    document.body.style.overflow = 'hidden'
-  }
-
-  const leaveControll = () => {
-    document.body.style.overflow = 'auto'
-  }
-
-  const handleMouseDown = useCallback((e: MouseEvent) => {
-    setIsDragging(true)
-    setInitialX(e.clientX)
-  }, [])
-
-  const handleMouseUp = useCallback(() => {
-    setIsDragging(false)
-  }, [])
-
-  const handleMouse = useCallback(
-    (e: MouseEvent) => {
-      if (isDragging) {
-        const delta = e.clientX - initialX
-        handleSetCount(delta)
-        setInitialX(e.clientX)
-      }
-    },
-    [handleSetCount, initialX, isDragging]
-  )
+    return setApplyCount(Math.round(Math.min(bodySize * scale, maxCountSize)))
+  }, [entryCount, maxCountSize, sizes, target])
 
   const handleTouchStart = useCallback((e: TouchEvent) => {
     const touch = e.touches[0]
+    setVisible(true)
+    setHasMoving(false)
     setTouchStartX(touch.clientX)
   }, [])
 
   const handleTouchMove = useCallback(
     (e: TouchEvent) => {
-      const touch = e.touches[0]
-      const delta = touch.clientX - touchStartX
-      handleSetCount(delta)
-      setTouchStartX(touch.clientX)
+      if (applyCount <= maxCountSize) {
+        const touch = e.touches[0]
+        const delta = touch.clientX - touchStartX
+        handleSetCount(delta * 8)
+        setTouchStartX(touch.clientX)
+        setHasMoving(true)
+      }
     },
-    [handleSetCount, touchStartX]
+    [applyCount, handleSetCount, maxCountSize, touchStartX]
   )
 
-  const handleSliderMouseDown = useCallback(
-    (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-      const sliderBar = e.currentTarget
-      const rect = sliderBar.getBoundingClientRect()
-      const clickedX = e.clientX - rect.left
-      const sliderWidth = sliderBar.clientWidth
-      const newValue = (clickedX / sliderWidth - 0.5) * size * 4
-      handleClickCount(newValue)
+  const handleReset = useCallback(() => {
+    !hasMoving && setEntryCount(0)
+  }, [hasMoving])
+
+  const handleMouseDown = useCallback((e: MouseEvent) => {
+    setIsDragging(true)
+    setVisible(true)
+    setHasMoving(false)
+    setInitialX(e.clientX)
+  }, [])
+
+  const handleMouseMove = useCallback(
+    (e: MouseEvent) => {
+      if (isDragging && applyCount <= maxCountSize) {
+        const delta = e.clientX - initialX
+        handleSetCount(delta * 8)
+        setInitialX(e.clientX)
+        setHasMoving(true)
+      }
     },
-    [handleClickCount, size]
+    [applyCount, handleSetCount, initialX, isDragging, maxCountSize]
   )
 
+  const handleMouseUp = useCallback(() => {
+    setIsDragging(false)
+    setVisible(false)
+  }, [])
+
+  // The entry of core handlers.
   useEffect(() => {
-    const controle = ref1.current as HTMLDivElement
-    const box = ref2.current as HTMLDivElement
-    const handle = ref3.current as HTMLDivElement
-    controle.addEventListener('wheel', handleWheel, { passive: true })
-    box.addEventListener('wheel', handleWheel, { passive: true })
-    box.addEventListener('mousedown', handleMouseDown)
-    box.addEventListener('touchstart', handleTouchStart)
+    const controle = refParent.current as HTMLDivElement
+    const box = refSwipe.current as HTMLDivElement
+
+    controle.addEventListener('touchstart', handleTouchStart)
     box.addEventListener('touchmove', handleTouchMove)
-    handle.addEventListener('touchstart', handleTouchStart)
-    handle.addEventListener('touchmove', handleTouchMove)
-    handle.addEventListener('mousedown', handleMouseDown)
+    controle.addEventListener('touchend', handleReset)
+
+    controle.addEventListener('mouseup', handleReset)
+    controle.addEventListener('mousedown', handleMouseDown)
+    document.addEventListener('mousemove', handleMouseMove)
     document.addEventListener('mouseup', handleMouseUp)
-    document.addEventListener('mousemove', handleMouse)
 
     return () => {
-      controle.removeEventListener('wheel', handleWheel)
-      box.removeEventListener('wheel', handleWheel)
-      box.removeEventListener('mousedown', handleMouseDown)
-      box.removeEventListener('touchstart', handleTouchStart)
+      // mobile handle.
+      controle.removeEventListener('touchstart', handleTouchStart)
       box.removeEventListener('touchmove', handleTouchMove)
-      handle.removeEventListener('touchstart', handleTouchStart)
-      handle.removeEventListener('touchmove', handleTouchMove)
-      handle.removeEventListener('mousedown', handleMouseDown)
+      controle.removeEventListener('touchend', handleReset)
+
+      // desktop handle.
+      controle.removeEventListener('mouseup', handleReset)
+      controle.removeEventListener('mousedown', handleMouseDown)
+      document.removeEventListener('mousemove', handleMouseMove)
       document.removeEventListener('mouseup', handleMouseUp)
-      document.removeEventListener('mousemove', handleMouse)
     }
   }, [
-    handleWheel,
     handleMouseDown,
+    handleMouseMove,
     handleMouseUp,
-    handleTouchStart,
+    handleReset,
     handleTouchMove,
-    handleMouse
+    handleTouchStart
   ])
 
+  // The controll start or end.
   useEffect(() => {
-    const controle = ref1.current as HTMLDivElement
+    document.body.style.cursor = isDragging ? 'grabbing' : 'auto'
+
+    const enterControll = (e: Event) => {
+      e.preventDefault()
+      document.body.style.overflow = 'hidden'
+    }
+
+    const leaveControll = () => {
+      document.body.style.overflow = 'auto'
+    }
+
+    const controle = refParent.current as HTMLDivElement
     controle.addEventListener('mouseover', enterControll, { passive: false })
     controle.addEventListener('mouseout', leaveControll)
     controle.addEventListener('touchstart', enterControll, { passive: false })
@@ -225,54 +209,36 @@ export const TextScaler = ({
       controle.removeEventListener('touchstart', enterControll)
       controle.removeEventListener('touchend', leaveControll)
     }
-  }, [handleMouse, handleMouseDown, handleMouseUp])
+  }, [isDragging])
 
   const [visible, setVisible] = useState(false)
-  const classes1 = className + ' ' + styles.t
-  const classes2 =
+
+  // entry classes group.
+  const classesPparent = className + ' ' + styles.parent
+  const classesBox = classBox + ' ' + styles.box
+
+  const classesView =
     styles.center + ' ' + (visible ? styles.visible : styles.hidden)
 
+  // The return the next React component.
   return (
-    <div
-      ref={ref1}
-      className={classes1}
-      onMouseEnter={() => setVisible(true)}
-      onMouseLeave={() => {
-        setVisible(false)
-
-        setIsDragging(false)
-      }}
-    >
-      T
-      <div
-        onMouseDown={handleSliderMouseDown}
-        ref={ref2}
-        style={{
-          width: visible ? 128 : 0,
-          height: visible ? Math.abs(sliderPosition) + 48 : 0
-        }}
-        className={classes2}
-      >
-        <div
-          style={{
-            background: sliderColor?.color,
-            border: '1px solid ' + sliderColor?.borderColor,
-            marginTop: sliderPosition,
-            pointerEvents: visible ? 'auto' : 'none'
-          }}
-          className={styles.slider}
-        >
-          <div
-            ref={ref3}
-            style={{
-              background: sliderColor?.handleColor,
-              border: '1px solid ' + sliderColor?.handleBorderColor,
-              transform: `translateX(${count * scaleFactor}px)`,
-              pointerEvents: visible ? 'auto' : 'none'
-            }}
-            className={styles.sliderHandle}
-          />
+    <div draggable="false" ref={refParent} className={classesPparent}>
+      {!visible && <div className={classesBox}>T</div>}
+      {visible && (
+        <div className={styles.counter}>
+          {applyCount}
+          <span className={styles.px}>px</span>
         </div>
+      )}
+      <div ref={refSwipe} className={classesView}>
+        <span style={{ marginRight: visible ? 8 : 0 }}>T</span>
+        <div className={classSlider} />
+        <span
+          className={styles.sizeBigT}
+          style={{ marginLeft: visible ? 8 : 0 }}
+        >
+          T
+        </span>
       </div>
     </div>
   )
